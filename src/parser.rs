10c1beee4fe70997ff7fa057c120dee7731fa80e
Lukasz Szpakowski <luckboy@vp.pl>
2022-06-16 15:35:09 +0200

    Amended code.

--- src/parser.rs
+++ src/parser.rs
@@ -15,8 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-use std::fmt;
 use std::cell::*;
+use std::fmt;
 use std::io::*;
 use std::rc::*;
 use std::result;
