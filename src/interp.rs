46695187c928ee0d32e5b21edbd0977d266f3c4a
Lukasz Szpakowski <luckboy@vp.pl>
2022-07-01 06:50:37 +0200

    Moved use.

--- src/interp.rs
+++ src/interp.rs
@@ -15,8 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-use std::collections::HashMap;
 use std::cell::*;
+use std::collections::HashMap;
 use std::fs::*;
 use std::io::*;
 use std::path;
